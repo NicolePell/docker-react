7fdeac3032c4db9372f89e7a26cadbf990bdc908
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2018-09-23 18:39:52 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          It is at this point that normal language gives up, and goes and has a drink.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
